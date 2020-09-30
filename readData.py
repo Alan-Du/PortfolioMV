1922081663bf07a954e089cd2d4be7af917e11ad
Shaolun Du <shaolun.du@gmail.com>
2020-09-30 08:09:46 +0800

    readDataUpdate

--- readData.py
+++ readData.py
@@ -6,6 +6,8 @@ local database after user setting up.
 @author: Shaolun Du
 @contact: shaolun.du@gmail.com
 """
+import pandas as pd
+from scipy.stats.mstats import gmean
 
 def readData(param):
     # readData funtion will look
@@ -17,10 +19,38 @@ def readData(param):
     #       "Rets":np.matrix, daily returns
     #       "dates":np.array, trading days
     #       "COV":dict of [date:np.matrix], cov matrix
-    #       "ER": np.matrix, expected returns
+    #       "ER":np.matrix, expected returns
     #       "rebaldates": np.array, rebalance dates
     #   }
-    fileName  = param['datasource']
+    ret_dict = {}
+    fileName  = param['datasource']+".xlsx"
     rebalFreq = param['rebalFreq']
+    period = int(rebalFreq[:-1])
+    unit = rebalFreq[-1]
+    if unit=="M":
+        # assuming 22 trading days in a month
+        rebal = period*22
+    elif unit=="D":
+        rebal = period
+    else:
+        raise Exception("please input a vaild rebalance period...")
+        
+    # sort datetime index in ascending order
+    df = pd.read_excel(fileName, index_col=0).sort_index()
+    Rets = df.pct_change()
+    secnames = df.columns.to_numpy()
+    dates = df.index.to_numpy()
+    if param['ERmethod']=="hist":
+        # epected return based on historical average
+        ER = Rets.rolling(window=rebal).apply(gmean)
+    if param['ERmethod']=="bl":
+        # to be implemented
+        ER = Rets.rolling(window=rebal).apply(gmean)
     
     return None
+
+
+####
+param = {"datasource":"Data",
+         "rebalFreq":"1M"}
+readData(param)
